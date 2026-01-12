bee5fd62c5b911f78b69ed97a50d108b658559eb
MORITANI Genta <n145004h@mails.cc.ehime-u.ac.jp>
2026-01-12 22:40:24 +0900

    デリート追加

--- src/App.tsx
+++ src/App.tsx
@@ -14,16 +14,22 @@ function App() {
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    const content = window.prompt("Todo content");
+    if (content) client.models.Todo.create({ content });
   }
-
+  
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
+            {todo.content}
+          </li>
         ))}
       </ul>
       <div>
